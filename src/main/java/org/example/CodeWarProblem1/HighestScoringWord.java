652502ed6e432a0e9e9ba147a6a23a36d6f22559
JLugo4 <jarek.lugo96@gmail.com>
2023-11-20 18:32:20 -0500

    CodeWar practice

---
+++ src/main/java/org/example/CodeWarProblem1/HighestScoringWord.java
@@ -0,0 +1,6 @@
+package org.example.CodeWarProblem1;public class HighestScoringWord {
+    public static String high(String aaB) {
+        String o = null;
+        return o;
+    }
+}
---
+++ src/test/java/org/example/HighestScoringWordTest/SolutionTest.java
@@ -0,0 +1,23 @@
+package org.example.HighestScoringWordTest;
+
+import org.example.CodeWarProblem1.HighestScoringWord;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void sampleTests() {
+        assertEquals("taxi", HighestScoringWord.high("man i need a taxi up to ubud"));
+        assertEquals("volcano", HighestScoringWord.high("what time are we climbing up to the volcano"));
+        assertEquals("semynak", HighestScoringWord.high("take me to semynak"));
+    }
+
+    @Test
+    public void edgeCaseTests() {
+        assertEquals("aa", HighestScoringWord.high("aa b"));
+        assertEquals("b", HighestScoringWord.high("b aa"));
+        assertEquals("bb", HighestScoringWord.high("bb d"));
+        assertEquals("d", HighestScoringWord.high("d bb"));
+        assertEquals("aaa", HighestScoringWord.high("aaa b"));
+    }
+}
